f6fbfcece3eca2419eb52dabfa57156b55ee00b1
CatalinaMat <167518614+CatalinaMat@users.noreply.github.com>
2024-05-10 16:00:01 +0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var button1 = document.getElementById('firstButton');
+button1.addEventListener('click', function () {
+	alert('Castigator!');
+})
+var button2 = document.getElementById('secondButton');
+button2.addEventListener('click', function () {
+	alert('Necastigator!');
+})
+var button3 = document.getElementById('thirdButton');
+button3.addEventListener ('click', function () {
+	alert('Necastigator');
+})
